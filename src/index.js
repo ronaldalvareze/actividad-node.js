82d998be2bcd9e606c37a026a5304eb64f370c6c
ronaldalvareze <127575929+ronaldalvareze@users.noreply.github.com>
2023-08-11 00:10:40 -0500

    actividad de node

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import {dirname, join} from 'path'
+import { fileURLToPath } from "url"
+import indexRoutes from  './routes/index.js'
+
+const app = express()
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+console.log(_dirname, 'views',)
+app.set('views', join(_dirname, 'views'))
+app.set('view engine' , 'ejs')
+app.use(indexRoutes)
+app.use(express.static(join(_dirname, 'public')))
+
+
+
+app.listen(3000
+console.log("servidor online", 3000);
