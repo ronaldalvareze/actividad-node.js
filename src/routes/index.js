d67869a515e4ed25f9df0f2b148c42d88620f5b2
ronaldalvareze <127575929+ronaldalvareze@users.noreply.github.com>
2023-08-24 14:52:29 -0500

    correcion error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-import{Router} from "express"
+import {  Router} from "express"
 const router = Router()
 
-router.get('/',(req, res) => res.render('index' , {title:'Este es el index'}))
-router.get('/about',(req, res) => res.render('about' , {title:'Este es el about  me'}))
-router.get('/service',(req, res) => res.render('service' , {title:'Este es el service'}))
-router.get('/portafolio',(req, res) => res.render('portafolio' , {title:'Este es el portafolio'}))
-router.get('/contact',(req, res) => res.render('contact' , {title:'Este es el about  me'}))
-router.get('/testimony',(req, res) => res.render('testimony' , {title:'Este es el testimony '}))
+
+
+router.get('/',(req, res) => res.render('index' , {title: 'este es el index'}))
+router.get('/about',(req, res) => res.render('about' , {title: 'este es about me '}))
+router.get('/contact',(req, res) => res.render('contact' , {title: 'este es el contact'}))
+
 
 export default router
